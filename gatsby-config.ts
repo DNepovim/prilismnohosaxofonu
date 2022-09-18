0d91f69a28911f5f0b7f96a28ec407074a89cb11
Dominik Bláha <dominik.blaha@avast.com>
2022-09-18 16:02:49 +0200

    Optimize loaded fonts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -31,11 +31,7 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [
-          `Reem+Kufi+Fun:wght@400;500;600;700`,
-          `Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900`,
-          `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
-        ],
+        fonts: [`Reem+Kufi+Fun:wght@500;`, `Montserrat:ital,wght@300;0,600;`],
         display: "swap",
       },
     },
