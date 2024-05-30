af1e4318c28dcfaeb7cae0ae11b16eb00182d5dc
Dominik Bláha <dominik.blaha@eman.cz>
2024-05-31 00:19:18 +0200

    Redesign contact block

--- src/blocks/ContactBlock.tsx
+++ src/blocks/ContactBlock.tsx
@@ -17,22 +17,27 @@ export interface ContactBlockProps extends BlockProps {
   }[]
 }
 
-export const ContactBlock: React.FC<ContactBlockProps> = ({
-  id,
-  title,
-  contacts,
-}) => (
+export const ContactBlock: React.FC<ContactBlockProps> = ({ id, title, contacts }) => (
   <Section id={id} title={title}>
-    <p>
+    <Flex>
       {contacts.map(({ type, text, link }, index) => (
         <CenteredLink href={link} target="_blank" key={index}>
           <ContactIcon type={type} />
           {text}
         </CenteredLink>
       ))}
-    </p>
+    </Flex>
   </Section>
 )
+
+const Flex = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 2rem;
+  @media (max-width: 640px) {
+    flex-direction: column;
+  }
+`
 
 const ContactIcon: React.FC<{ type: ContactType }> = ({ type }) => {
   switch (type) {
@@ -46,4 +51,22 @@ const ContactIcon: React.FC<{ type: ContactType }> = ({ type }) => {
 const CenteredLink = styled.a`
   display: flex;
   align-items: center;
+  flex-direction: column;
+  font-weight: bold;
+  gap: 0.6em;
+  min-width: 12rem;
+  padding: 1rem;
+  border: 6px solid transparent;
+  transition: border-color 300ms, background-color 900ms;
+
+  &:hover {
+    border-color: #d3a85350;
+    border-radius: 10px;
+    background-color: #ffffff30;
+  }
+
+  svg {
+    width: 4em;
+    height: 4rem;
+  }
 `
